575922f0d628c8c171c8be376169c0269fd5446f
Quentin Maginot <quentin.maginot@gmail.com>
2012-11-21 07:38:57 +0100

    creation bases rails + modif index.html (hello wolrd)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DocTor::Application.config.secret_token = '973b2e15db2f54e227613dc84ede5ceb70fbe658bf00b118d122ff31e4492b0b287c52e00fa6db2e248bf745def3b7d61a6c96f2bb4cb14d28253273b364c3a0'
